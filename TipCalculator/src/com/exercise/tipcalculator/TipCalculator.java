1a337105b260711575fd065f221a4014a62415c5
srichara <srichara@yahoo-inc.com>
2014-06-09 20:19:42 -0700

    Adding the slider to determine percentage and updating tip amount accordingly

--- TipCalculator/src/com/exercise/tipcalculator/TipCalculator.java
+++ TipCalculator/src/com/exercise/tipcalculator/TipCalculator.java
@@ -4,35 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class TipCalculator extends Activity {
 
-	private static Button lastClickedButton;
+	private static int percentValue;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tip_calculator);
+		setPercentValue();
 		setUpBillAmountTextChangeListener();
-		
-	}
-	
-	public void onClickTipAmountTenPercent(View v) {
-		lastClickedButton = (Button) findViewById(R.id.tenPercentId);
-		setTipAmount();
-	}
-	
-	public void onClickTipAmountFifteenPercent(View v) {
-		lastClickedButton = (Button) findViewById(R.id.fifteenPercentId);
-		setTipAmount();
-	}
-	
-	public void onClickTipAmountTwentyPercent(View v) {
-		lastClickedButton = (Button) findViewById(R.id.twentyPercentId);
-		setTipAmount();
+		setUpSeekBarChangeListener();
 	}
 	
 	private void setUpBillAmountTextChangeListener() {
@@ -59,34 +44,46 @@ public class TipCalculator extends Activity {
 	    });
 	}
 	
+	private void setUpSeekBarChangeListener() {
+		SeekBar percentageBar = (SeekBar) findViewById(R.id.tipPercentageId);
+		percentageBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+		   @Override 
+		   public void onProgressChanged(SeekBar seekBar, int progress, 
+		     boolean fromUser) { 
+			   percentValue = progress;
+			   setPercentValue();
+			   setTipAmount();
+		   } 
+
+		   @Override 
+		   public void onStartTrackingTouch(SeekBar seekBar) { 
+		    // TODO Auto-generated method stub 
+		   } 
+
+		   @Override 
+		   public void onStopTrackingTouch(SeekBar seekBar) { 
+		    // TODO Auto-generated method stub 
+		   } 
+       }); 
+   } 
+	
+	private void setPercentValue() {
+		TextView percentageValueText = (TextView) findViewById(R.id.percentageValueTextId);
+		   percentageValueText.setText(percentValue + "%");
+	}
+	
 	private void setTipAmount() {
 		TextView billAmountText = (TextView) findViewById(R.id.billAmtId);
 		TextView computedAmtText = (TextView) findViewById(R.id.computedAmountId);
-		double percentValue = 0;
-		if (null != lastClickedButton && null != billAmountText.getText()) {
-			switch(lastClickedButton.getId()) {
-				case R.id.tenPercentId:
-					percentValue = 0.1;
-					break;
-					
-				case R.id.fifteenPercentId:
-					percentValue = 0.15;
-					break;
-					
-				case R.id.twentyPercentId:
-					percentValue = 0.2;
-					break;
-			}
-			computedAmtText.setText("");
-			String billAmount = billAmountText.getText().toString();
-	    	String computedTip = formatCurrency(billAmount, percentValue);
-	    	computedAmtText.setText(computedTip);    
-		}
+		computedAmtText.setText("");
+		String billAmount = billAmountText.getText().toString();
+    	String computedTip = formatCurrency(billAmount, percentValue);
+    	computedAmtText.setText(computedTip);
 
 	}
 	
 	private String formatCurrency(String billAmount, double percentValue) {
-		double computedTip = Float.valueOf(billAmount) * percentValue;
+		double computedTip = Float.valueOf(billAmount) * (percentValue/100);
 		String formattedAmount;
         formattedAmount = String.format("$%.2f", computedTip);
 		return formattedAmount;
