53a74da72be9ffd5e030e59da709f66a7a322b01
srichara <srichara@yahoo-inc.com>
2014-06-22 21:21:22 -0700

    Committing files for compose and infinite scroll

--- android-rest-client-template-master/src/com/codepath/apps/basic/twitter/TimelineActivity.java
+++ android-rest-client-template-master/src/com/codepath/apps/basic/twitter/TimelineActivity.java
@@ -3,10 +3,15 @@ package com.codepath.apps.basic.twitter;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,27 +30,70 @@ public class TimelineActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
 		client = TwitterApplication.getRestClient();
-		populateTimeline();
 		lvTweets = (ListView)findViewById(R.id.lvTweets);
 		tweets = new ArrayList<Tweet>();
 		aTweets = new TweetArrayAdapter(this, tweets);
 		lvTweets.setAdapter(aTweets);
+		populateTimeline(-1);
+		
+		 // Attach the listener to the AdapterView onCreate
+		lvTweets.setOnScrollListener(new EndlessScrollListener() {
+		    @Override
+		    public void onLoadMore(int page, int totalItemsCount) {
+	                // Triggered only when new data needs to be appended to the list
+	                // Add whatever code is needed to append new items to your AdapterView
+		    	populateTimeline(getMinId() - 1); 
+		    }
+	    });
 	}
 	
-	public void populateTimeline(){
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tweets, menu);
+		return true;
+	}
+	
+	public void populateTimeline(long maxId){
 		client.getHomeTimeLine(new JsonHttpResponseHandler(){
 			@Override
 			public void onSuccess(JSONArray array) {
+				Log.d("debug ", array.toString());
 				aTweets.addAll(Tweet.fromJsonArray(array));
 				
 			}
-			
 			
 			@Override
 			public void onFailure(Throwable e, String s) {
 				Log.d("debug ", e.toString());
 				Log.d("debug ", e.toString());
 			}
-		});
+		}, maxId);
+	}
+	
+	public long getMinId() {
+		return aTweets.getItem(aTweets.getCount()-1).getUid();
+	}
+	
+	
+	public void onComposeClick(MenuItem mi) {
+		Intent i = new Intent(this, ComposeActivity.class);
+		startActivityForResult(i, 50);
+    }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent i) {
+ 		if (resultCode == RESULT_OK) {
+ 			if (requestCode == 50) {
+ 				JSONObject jsonTweet;
+ 				try {
+ 					jsonTweet = new JSONObject(i.getExtras().getString("jsonTweet"));
+ 					aTweets.add(Tweet.fromJson(jsonTweet));
+ 				} catch (JSONException e) {
+ 					e.printStackTrace();
+ 					return;
+ 				}
+ 			}
+ 		}
 	}
 }
--- android-rest-client-template-master/src/com/codepath/apps/basic/twitter/TweetArrayAdapter.java
+++ android-rest-client-template-master/src/com/codepath/apps/basic/twitter/TweetArrayAdapter.java
@@ -1,8 +1,13 @@
 package com.codepath.apps.basic.twitter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +19,8 @@ import com.codepath.apps.basic.twitter.models.Tweet;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
-
+	final String TWITTER="EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+	
 	public TweetArrayAdapter(Context context, List<Tweet> tweets) {
 		super(context, 0, tweets);
 	}
@@ -33,12 +39,35 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 		ImageView ivProfileImage = (ImageView) view.findViewById(R.id.ivProfileImage);
 		TextView tvUserName = (TextView) view.findViewById(R.id.tvUserName);
 		TextView tvBody = (TextView) view.findViewById(R.id.tvBody);
+		TextView tvRelativeTime = (TextView) view.findViewById(R.id.tvRelativeTime);
+		Date date;
+		long milliseconds = 0;
+		try {
+			date = new SimpleDateFormat(TWITTER, Locale.ENGLISH).parse(tweet.getCreatedAt());
+			milliseconds = date.getTime();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			date = null;
+		}
+				
+		CharSequence relativeDate = DateUtils.getRelativeDateTimeString(
+		        this.getContext(), // Suppose you are in an activity or other Context subclass
+		        milliseconds,
+		        DateUtils.MINUTE_IN_MILLIS, // The resolution. This will display only 
+		                                        // minutes (no "3 seconds ago") 
+		        DateUtils.WEEK_IN_MILLIS, // The maximum resolution at which the time will switch 
+		                         // to default date instead of spans. This will not 
+		                         // display "3 weeks ago" but a full date instead
+		        0); // Eventual flags
+
 		
 		ivProfileImage.setImageResource(android.R.color.transparent);
 		ImageLoader imageLoader = ImageLoader.getInstance();
 		imageLoader.displayImage(tweet.getUser().getProfileImageUrl(), ivProfileImage);
 		tvUserName.setText(tweet.getUser().getScreenName());
 		tvBody.setText(tweet.getBody());
+		tvRelativeTime.setText(relativeDate);
 		return view;
 	}
 	
